ae3c5f88e764096e5a6adf4b9678b7bb948a3880
Misty Becker <mistyb01@uw.edu>
2022-10-13 16:43:55 -0700

    css grid setup

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -2,9 +2,29 @@ import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>heehee</p>
-    </div>
+    <main>
+      <input type="text" className="calculator-window"></input>
+      <div className="calculator-grid">
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>del</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button>/</button>
+        <button>x</button>
+        <button className="grid-col-span-2">reset</button>
+        <button className="grid-col-span-2">=</button>
+      </div>
+    </main>
   );
 }
 
