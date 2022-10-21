aea46d61ed101ba275bf163d88c0c6475753e759
Misty Becker <mistyb01@uw.edu>
2022-10-20 18:58:44 -0700

    theme colors (besides active states) work!

--- calculator/README-template.md
+++ calculator/README-template.md
@@ -1,6 +1,6 @@
 # Frontend Mentor - Calculator app solution
 
-This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
 ## Table of contents
 
@@ -35,7 +35,7 @@ Users should be able to:
 
 Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
 
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
+Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it.
 
 Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
 
@@ -49,6 +49,7 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 ## My process
 
 ### Built with
+
 - CSS custom properties
 - Flexbox
 - CSS Grid
@@ -65,15 +66,17 @@ To see how you can add code snippets, see below:
 ```html
 <h1>Some HTML code I'm proud of</h1>
 ```
+
 ```css
 .proud-of-this-css {
   color: papayawhip;
 }
 ```
+
 ```js
 const proudOfThisFunc = () => {
-  console.log('🎉')
-}
+  console.log("🎉");
+};
 ```
 
 If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
@@ -103,6 +106,4 @@ Use this section to outline areas that you want to continue focusing on in futur
 
 ## Acknowledgments
 
-This is where you can give a hat tip to anyone who helped you out on this project. Perhaps you worked in a team or got some inspiration from someone else's solution. This is the perfect place to give them some credit.
-
-**Note: Delete this note and edit this section's content as necessary. If you completed this challenge by yourself, feel free to delete this section entirely.**
+For the theme slider, I used a similar approach to Angelos Arnis' [codepen(http://cssdeck.com/labs/ufct35ys5t)], from a [question on StackExchange(https://ux.stackexchange.com/questions/75399/how-should-a-multi-state-toggle-slider-with-more-than-two-states-be-implemented)]
--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,5 +1,5 @@
 import './index.css';
-import { useReducer } from 'react';
+import { useReducer, useState, useEffect } from 'react';
 
 function reducer(state, action) {
   switch (action.type) {
@@ -23,10 +23,91 @@ function reducer(state, action) {
   throw Error('Unknown action.');
 }
 
+
 function App() {
   // state = the initial state (input = 0)
   // dispatch = the function that lets you change the value in response to an action (reducer)
   const [state, dispatch] = useReducer(reducer, {input: ''});
+  const [theme, setTheme] = useState(1);
+  let root = document.documentElement;
+
+  useEffect(() => {
+    switch(theme) {
+      case 1:
+        root.style.setProperty('--body-text-color', 'white');
+        root.style.setProperty('--text-key', 'hsl(221, 14%, 31%)');
+
+        root.style.setProperty('--main-bg', 'hsl(222, 26%, 31%)');
+        root.style.setProperty('--keypad-bg', 'hsl(223, 31%, 20%)');
+        root.style.setProperty('--screen-bg', 'hsl(224, 36%, 15%)');
+
+        root.style.setProperty('--key-bg', 'hsl(32, 26%, 89%)');
+        root.style.setProperty('--key-shadow', 'hsl(28, 16%, 65%)');
+        root.style.setProperty('--key-bg-active', 'white');
+
+        root.style.setProperty('--key-alt-bg', 'hsl(225, 21%, 49%)');
+        root.style.setProperty('--key-alt-shadow', 'hsl(224, 28%, 35%)');
+        root.style.setProperty('--key-alt-active', 'hsl(224, 51%, 76%)');
+
+        root.style.setProperty('--key-equals-bg', 'hsl(6, 63%, 50%)');
+        root.style.setProperty('--key-equals-shadow', 'hsl(6, 70%, 34%)');
+        root.style.setProperty('--key-equals-active', 'hsl(6, 93%, 67%)');
+        root.style.setProperty('--equals-text-color', 'white');
+
+
+        root.style.setProperty('--toggle-color', 'hsl(6, 63%, 50%)');
+
+        break;
+      case 2:
+        root.style.setProperty('--body-text-color', 'hsl(60, 10%, 19%)');
+        root.style.setProperty('--text-key', 'hsl(60, 10%, 19%)');
+
+        root.style.setProperty('--main-bg', 'hsl(0, 0%, 90%)');
+        root.style.setProperty('--keypad-bg', 'hsl(0, 5%, 81%)');
+        root.style.setProperty('--screen-bg', 'hsl(0, 0%, 93%)');
+
+        root.style.setProperty('--key-bg', 'hsl(45, 7%, 89%)');
+        root.style.setProperty('--key-shadow', 'hsl(35, 11%, 61%)');
+        root.style.setProperty('--key-bg-active', 'hsl(6, 93%, 67%)');
+
+        root.style.setProperty('--key-equals-bg', 'hsl(25, 98%, 40%)');
+        root.style.setProperty('--key-equals-shadow', 'hsl(25, 99%, 27%)');
+        root.style.setProperty('--key-equals-active', 'hsl(224, 51%, 76%)');
+        root.style.setProperty('--equals-text-color', 'white');
+
+        root.style.setProperty('--key-alt-bg', 'hsl(185, 42%, 37%)');
+        root.style.setProperty('--key-alt-shadow', 'hsl(185, 58%, 25%)');
+        root.style.setProperty('--key-alt-active', 'hsl(6, 93%, 67%)');
+
+        root.style.setProperty('--toggle-color', 'hsl(25, 98%, 40%)');
+        break;
+      case 3:
+        root.style.setProperty('--body-text-color', 'hsl(52, 100%, 62%)');
+        root.style.setProperty('--text-key', 'hsl(52, 100%, 62%)');
+
+        root.style.setProperty('--main-bg', 'hsl(268, 75%, 9%)');
+        root.style.setProperty('--keypad-bg', 'hsl(268, 71%, 12%)');
+        root.style.setProperty('--screen-bg', 'hsl(268, 71%, 12%)');
+
+        root.style.setProperty('--key-bg', 'hsl(281, 89%, 26%)');
+        root.style.setProperty('--key-shadow', 'hsl(285, 91%, 52%)');
+        root.style.setProperty('--key-bg-active', 'hsl(6, 93%, 67%)');
+
+        root.style.setProperty('--key-equals-bg', 'hsl(176, 100%, 44%)');
+        root.style.setProperty('--key-equals-shadow', 'hsl(177, 92%, 70%)');
+        root.style.setProperty('--key-equals-active', 'hsl(224, 51%, 76%)');
+        root.style.setProperty('--equals-text-color', 'hsl(198, 20%, 13%)');
+
+        root.style.setProperty('--key-alt-bg', 'hsl(268, 47%, 21%)');
+        root.style.setProperty('--key-alt-shadow', 'hsl(290, 70%, 36%)');
+        root.style.setProperty('--key-alt-active', 'hsl(6, 93%, 67%)');
+
+        root.style.setProperty('--toggle-color', 'hsl(176, 100%, 44%)');
+        break;
+    }
+
+  }, [theme]);
+  
 
   const CalculatorButton = ({value}) => {
     const handleClick = (e) => {
@@ -62,6 +143,15 @@ function App() {
     : <button onClick = {handleClick}>{value}</button>
   }
 
+  function changeTheme(e) {
+    if (e.target.id === 'first_toggle') {
+      setTheme(1);
+    } else if (e.target.id === 'second_toggle') {
+      setTheme(2);
+    } else {
+      setTheme(3);
+    }
+  }
   return (
     <main>
       <div className="heading">
@@ -75,9 +165,9 @@ function App() {
               <span>3</span>
             </div>
             <fieldset>
-              <input type="radio" class="toggle_option" id="first_toggle" name="toggle_option"/>
-              <input type="radio" class="toggle_option" id="second_toggle" name="toggle_option"/>
-              <input type="radio" class="toggle_option" id="third_toggle" name="toggle_option"/>
+              <input type="radio" class="toggle_option" id="first_toggle" name="toggle_option" onClick={changeTheme}/>
+              <input type="radio" class="toggle_option" id="second_toggle" name="toggle_option" onClick={changeTheme}/>
+              <input type="radio" class="toggle_option" id="third_toggle" name="toggle_option" onClick={changeTheme}/>
               <label for="first_toggle"></label>
               <label for="second_toggle"></label>
               <label for="third_toggle"></label>
